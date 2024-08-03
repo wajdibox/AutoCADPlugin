7860e7e6a3303793bb68f23298cc27b66fc066c3
wajdibox <wajdiboxabbassi@gmail.com>
2024-08-03 17:16:00 +0200

    Add simple utility functions for testing.

--- src/CommonUtil.vb
+++ src/CommonUtil.vb
@@ -1,3 +1,17 @@
-﻿Public Class CommonUtil
+﻿Namespace AutoCADPlugin.Utilities
 
-End Class
+    Public Class CommonUtil
+        ' A simple function to add two numbers
+        Public Function AddNumbers(a As Integer, b As Integer) As Integer
+            Return a + b
+        End Function
+
+        ' A class with a method to print a message
+        Public Class MessagePrinter
+            Public Sub PrintMessage()
+                Console.WriteLine("Hello, this is a test message from CommonUtil!")
+            End Sub
+        End Class
+    End Class
+
+End Namespace
