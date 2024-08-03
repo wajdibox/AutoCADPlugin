299261d650e888fd3b7b5b45940bad98209d5bf3
wajdibox <wajdiboxabbassi@gmail.com>
2024-08-03 17:49:52 +0200

    Adding New Branch

---
+++ docs/README.md
@@ -0,0 +1,1 @@
+This is the read me for the 'add-new-feature' branch
--- src/CommonUtil.vb
+++ src/CommonUtil.vb
@@ -1,17 +1,3 @@
-﻿Namespace AutoCADPlugin.Utilities
+﻿Public Class CommonUtil
 
-    Public Class CommonUtil
-        ' A simple function to add two numbers
-        Public Function AddNumbers(a As Integer, b As Integer) As Integer
-            Return a + b
-        End Function
-
-        ' A class with a method to print a message
-        Public Class MessagePrinter
-            Public Sub PrintMessage()
-                Console.WriteLine("Hello, this is a test message from CommonUtil!")
-            End Sub
-        End Class
-    End Class
-
-End Namespace
+End Class
